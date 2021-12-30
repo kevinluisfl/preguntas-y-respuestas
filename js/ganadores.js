7eeb45ab31eb0d19b6b6eb07ae0dfec1c5ec0a91
kevinluisfl <71282006+kevinluisfl@users.noreply.github.com>
2021-12-29 23:20:41 -0500

    listo: inicio, ganadores, instrucciones y parte de principal

---
+++ js/ganadores.js
@@ -0,0 +1,71 @@
+const datostorage = JSON.parse(localStorage.getItem('datos'));
+const tabla = document.querySelector('#tablaganadores');
+const btnBorrar = document.querySelector('#btnborrartodo');
+
+if(datostorage){
+    datostorage.sort((a,b)=>{
+        if(a.puntos < b.puntos){
+            return 1;
+        } else if(a.puntos > b.puntos){
+            return -1;
+        }else {
+            if(a.tiempo < b.tiempo){
+                return -1;
+            } else if(a.tiempo > b.tiempo){
+                return 1;
+            }else {
+                return 0;
+            }
+        }
+    })
+}
+
+console.log(datostorage);
+
+let creaTabla = function(datos){
+    let cabecera = `<tr><th>Usuario</th>
+                    <th>Puntos</th>
+                    <th>Tiempo (s)</th>
+                    <th>Fecha</th></tr>`;
+    if(datos){
+        for(let dato of datos){
+            let fila = `<tr>`;
+            fila += `<td>${dato.user}</td>`;
+            fila += `<td>${dato.puntos}</td>`;
+            fila += `<td>${dato.tiempo}</td>`;
+            fila += `<td>${dato.fecha}</td>`;
+            fila += `</tr>`
+            cabecera += fila;
+        }
+    }else{
+        cabecera += `No hay registros`;
+    }
+    return cabecera;
+}
+
+tabla.innerHTML = creaTabla(datostorage);
+
+btnBorrar.addEventListener("click", ()=>{
+    Swal.fire({
+        title: 'Borrar todos los datos?',
+        text: `Se vaciara todo el historial`,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, borrar todo!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          if(localStorage.getItem('datos') === null){
+            Swal.fire({
+                title: 'Se ha borrado todo!',
+                text: `No hay historial de datos`,
+                icon:'info',
+              })
+          }else{
+              localStorage.removeItem('datos');
+              window.location="./ganadores.html";
+          }
+        }
+      })
+})
--- js/preguntas.js
+++ js/preguntas.js
@@ -1,16 +1,47 @@
 const usuario = localStorage.getItem('user').toUpperCase();
+const datostorage = JSON.parse(localStorage.getItem('datos'));
+
 const titulo = document.querySelector('h1');
 const btnRetiro = document.querySelector('#btnretiro');
-const puntos = 0;
-const tiempo = 0;
+
+///contador de tiempo
+var t = document.querySelector('#tiempo');
+var n = 0;
+var ban = true;
+window.setInterval(function(){
+  if(ban){
+    n++;
+    t.innerHTML = n;
+    }
+},1000);
+////
+
+var historial=[];
+if(localStorage.getItem('datos') === null){
+    console.log("NO existe datos");
+}else{
+    historial = datostorage;
+}
+
+
+var datos = {
+    user: usuario,
+    puntos: 0,
+    tiempo: 0,
+    fecha: ''
+}
+
+var {user, puntos, tiempo, fecha} = datos;
 
 titulo.textContent= `Hola ${usuario}, Escoge la opción correcta`;
 
 btnRetiro.addEventListener("click", ()=>{
+  /////PREGUNTAR POR LOS PUNTOS, PARA QUE NO GUARDE CON 0 PTS
+  ////SI SE RETIRA EN LA PRIMERA LO LLEVE AL INICIO
     Swal.fire({
         title: 'Desea retirarse?',
         html: `El juego terminará con el siguiente acumulado: <br>
-        <b>${puntos} puntos</b> y un tiempo de <b>${tiempo} segundos</b>`,
+        <b>${puntos} puntos</b> y un tiempo de <b>${n} segundos</b>`,
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
@@ -18,13 +49,20 @@ btnRetiro.addEventListener("click", ()=>{
         confirmButtonText: 'Si, retirarme!'
       }).then((result) => {
         if (result.isConfirmed) {
-          Swal.fire({
-            title: 'Juego terminado!',
-            text: `Te has retirado ${usuario}`,
-            icon:'info',
-          })
+          ///parar contador
+          ban = false;
+          ///formatear fecha
+          var date = new Date();
+          datos= {...datos, tiempo: n, fecha: date.toLocaleString()};
+          ///pasar datos al historial
+          historial.push(datos);
+          localStorage.removeItem('datos');
+          ////pasar datos al localstorage
+          localStorage.setItem('datos', JSON.stringify(historial));
+          ////redireccionar al page ganadores
           window.location="./ganadores.html";
+          localStorage.removeItem('user');
         }
       })
-    localStorage.removeItem('user');
 })
+
